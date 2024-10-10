af036ab17dc5f1709946534ad217c0d16a94347e
AgentK <git@agentk.dev>
2024-10-09 22:31:16 -0500

    re-apply

---
+++ kubejs/server_scripts/bmp/veinmining.js
@@ -0,0 +1,8 @@
+ServerEvents.tags('item', event => {
+  event.add('veinmining:enchantable/vein_mining_tools', 'spectrum:bedrock_pickaxe')
+  event.add('veinmining:enchantable/vein_mining_tools', 'spectrum:bedrock_bow')
+  event.add('veinmining:enchantable/vein_mining_tools', 'spectrum:bedrock_axe')
+  event.add('veinmining:enchantable/vein_mining_tools', 'spectrum:bedrock_shovel')
+  event.add('veinmining:enchantable/vein_mining_tools', 'spectrum:bedrock_hoe')  
+  event.add('veinmining:enchantable/vein_mining_tools', 'spectrum:bedrock_shears')  
+})
