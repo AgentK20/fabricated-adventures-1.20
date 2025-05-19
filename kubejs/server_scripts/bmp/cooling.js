f5d5dcab73de8e4fc9141e8d5c576e9e76cb779b
AgentK <git@agentk.dev>
2025-05-19 02:08:59 -0500

    more cooling

--- kubejs/server_scripts/bmp/cooling.js
+++ kubejs/server_scripts/bmp/cooling.js
@@ -1,3 +1,8 @@
 ServerEvents.tags('item', event => {
   event.add('scorchful:is_cooling_food', 'bettermcdonaldsmod:lipton_ice_tea_punch')
+  event.add('scorchful:is_cooling_food', 'seeddelight:cherry_ice_cream')
+  event.add('scorchful:is_cooling_food', 'casualness_delight:green_tongue')
+  event.add('scorchful:is_cooling_food', 'ends_delight:chorus_fruit_popsicle')
+  event.add('scorchful:is_cooling_food', 'farmersdelight:melon_popsicle')
+  event.add('scorchful:is_cooling_food', 'moredelight:chocolate_popsicle')
 })
